a58745a4c6274f99a6e29c89cd0f1d2bfaa2a412
TobiasLino <tobias.lino@fatec.sp.gov.br>
2020-06-25 15:41:19 -0300

    cliente: lista de pets adicionada

--- src/Cliente.java
+++ src/Cliente.java
@@ -1,2 +1,25 @@
+import java.util.LinkedList;
+
 public class Cliente extends People {
+    private LinkedList<Animal> pets;
+
+    public Cliente() {
+        super();
+        pets = new LinkedList<>();
+    }
+
+    public void addPet(Animal pet) {
+        if (pet == null) {
+            System.out.println("Erro: Animal está nulo.");
+            return;
+        } else if (pets.size() >= 5) {
+            System.out.println("Erro: Só é possível cadastrar cinco pets para cada cliente.");
+            return;
+        }
+        pets.push(pet);
+    }
+
+    public void removePet(String name) {
+        
+    }
 }
